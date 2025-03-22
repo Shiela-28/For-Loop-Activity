7be679755471fe1acda353c4e9783595fed1b885
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-22 09:03:06 +0800

    Add files via upload

--- evenNumbers.js
+++ evenNumbers.js
@@ -1,5 +1,8 @@
-for (let i = 1; i <= 10; i++) {
-	if (i % 2 === 0) {
+// Loop through numbers 1 to 10.
+for (let i = 1; i <= 10; i++) {  //The for loop iterates from 1 to 10 (inclusive).
+	// Check if the number is even.
+	if (i % 2 === 0) { 
+	  // If even, print the number.
 	  console.log(i);
 	}
-  }
+  } 
